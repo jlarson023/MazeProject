39b186f79b1bd518e0b3bdc61145765924a1f45e
Tonson5 <62815448+Tonson5@users.noreply.github.com>
2024-02-20 10:05:29 -0800

    Basic transition done

---
+++ Assets/Evans Folder/Evans scripts/LevelTransition.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour
+{
+    public string nextLevel;
+    public SceneManager sceneManager;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+       
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nextLevel);
+        }
+        
+    }
+}
